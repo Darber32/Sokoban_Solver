68c0f461a5c0fcf1b4fcfe62589a36f98c0b17f4
Darber32 <sergey_sbrodov@list.ru>
2025-09-28 19:25:45 +0300

    Добавлены алгоритмы двунаправленного поиска и итерационного поиска в глубину

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ import os
 from queue import Queue, LifoQueue
 
 class State:
-    def __init__(self, player_pos, boxes, prev_state = None):
+    def __init__(self, player_pos, boxes, prev_state = None, depth = 0):
         self.player = player_pos
         self.boxes = frozenset(boxes)
         self.prev_state = prev_state
+        self.depth = depth
 
     def __eq__(self, other):
         if other is None:
@@ -37,8 +38,10 @@ class Game:
         self.status = 'menu'
         self.levels = [os.path.splitext(f)[0] for f in os.listdir('Levels') if f.endswith('.txt')]
         self.selected_level = None
-        self.structure_type = 'stack'
         self.level_index = 0
+
+        self.algorithms = ['depth-first search', 'iterative depth-first search', 'breadth-first search', 'bidirectional search']
+        self.algorithm_index = 0
 
         self.iteration_count = 0
         self.O_max_node_count = 0
@@ -64,7 +67,7 @@ class Game:
                     elif e.key == pygame.K_DOWN and self.status == 'menu':
                         self.level_index = (self.level_index + 1) % len(self.levels)
                     elif e.key == pygame.K_SPACE and self.status == 'menu':
-                        self.structure_type = 'queue' if self.structure_type == 'stack' else 'stack'
+                        self.algorithm_index = (self.algorithm_index + 1) % len(self.algorithms) 
                     elif e.key == pygame.K_RETURN:
                         if self.status == 'menu':
                             self.selected_level = self.levels[self.level_index] + '.txt'
@@ -83,7 +86,15 @@ class Game:
                     self.O_end_node_count = 0
                     self.max_node_count = 0
                     self.steps_counter = 0
-                    self.maps = self.Find_Solution(self.selected_level, self.structure_type)
+                    match self.algorithms[self.algorithm_index]:
+                        case 'depth-first search':
+                            self.maps = self.Find_Solution(self.selected_level, 'stack')
+                        case 'iterative depth-first search':
+                            self.maps = self.DFS_Iterative(self.selected_level)
+                        case 'breadth-first search': 
+                            self.maps = self.Find_Solution(self.selected_level, 'queue')
+                        case 'bidirectional search':
+                            self.maps = self.Bidirectional_Search(self.selected_level)
                     if self.maps == None:
                         self.status = 'error'
                     else:
@@ -189,6 +200,216 @@ class Game:
                         
         return None
 
+    def DFS_Iterative(self, level_name):
+        map_file = open('Levels/' + level_name, 'r') 
+        start_map = map_file.read().split(sep='\n')
+        map_file.close()
+        self.map_rows = len(start_map)
+        self.map_cols = len(start_map[0])
+        for i in range(self.map_rows):
+            start_map[i] = list(start_map[i])
+
+        player = None
+        boxes = list()
+        self.map.clear()
+        for y in range(self.map_rows):
+            row = list()
+            for x in range(self.map_cols):
+                if start_map[y][x] == '@':
+                    player = (x, y)
+                    row.append('.')
+                elif start_map[y][x] == '*':
+                    player = (x, y)
+                    row.append('X')
+                elif start_map[y][x] == 'B':
+                    boxes.append((x, y))
+                    row.append('.')
+                elif start_map[y][x] == '+':
+                    boxes.append((x, y))
+                    row.append('X')
+                else:
+                    row.append(start_map[y][x])
+            self.map.append(row)
+        start_state = State(player, boxes)
+        
+        final_state_file = open('Levels/Final States/' + level_name, 'r')
+        final_map = final_state_file.read().split(sep='\n')
+        final_state_file.close()
+        final_player = None
+        final_boxes = list()
+        for y in range(self.map_rows):
+            for x in range(self.map_cols):
+                if final_map[y][x] == '@':
+                    final_player = (x, y)
+                elif final_map[y][x] == '+':
+                    final_boxes.append((x, y))
+        final_state = State(final_player, final_boxes)
+
+        directions = ['up', 'down', 'right', 'left']
+        O = LifoQueue()
+        cur_depth = 1
+        max_depth = 10000
+        
+        while True: 
+            O.put(start_state)
+            C = {start_state: start_state.depth}
+            while not O.empty():
+                self.iteration_count += 1
+                state = O.get()
+                if state == final_state:
+                    self.O_end_node_count = O.qsize()
+                    maps = list()
+                    while state.prev_state != None:
+                        self.steps_counter += 1
+                        map = self.Create_Map(state)
+                        maps.append(map)
+                        state = state.prev_state
+                    map = self.Create_Map(state)
+                    maps.append(map)
+                    return maps    
+
+                if state.depth < cur_depth:
+                    for direction in directions:
+                        new_state = self.Check_Direction(direction, state)
+                        if new_state != None:
+                            new_state.depth = state.depth + 1
+                            if not new_state in C or new_state.depth < C[new_state]:
+                                O.put(new_state)
+                                C[new_state] = new_state.depth
+                                O_size = O.qsize()
+                                self.max_node_count = max(self.max_node_count, len(C) + O_size)
+                                self.O_max_node_count = max(self.O_max_node_count, O_size)   
+
+            cur_depth += 1 
+            if cur_depth >= max_depth:
+                break
+
+        return None
+
+
+    def Bidirectional_Search(self, level_name):
+        map_file = open('Levels/' + level_name, 'r') 
+        start_map = map_file.read().split(sep='\n')
+        map_file.close()
+        self.map_rows = len(start_map)
+        self.map_cols = len(start_map[0])
+        for i in range(self.map_rows):
+            start_map[i] = list(start_map[i])
+
+        player = None
+        boxes = list()
+        self.map.clear()
+        for y in range(self.map_rows):
+            row = list()
+            for x in range(self.map_cols):
+                if start_map[y][x] == '@':
+                    player = (x, y)
+                    row.append('.')
+                elif start_map[y][x] == '*':
+                    player = (x, y)
+                    row.append('X')
+                elif start_map[y][x] == 'B':
+                    boxes.append((x, y))
+                    row.append('.')
+                elif start_map[y][x] == '+':
+                    boxes.append((x, y))
+                    row.append('X')
+                else:
+                    row.append(start_map[y][x])
+            self.map.append(row)
+        start_state = State(player, boxes)
+
+        O_start = Queue()
+        O_start.put(start_state)
+        C_start = {start_state}
+        
+
+        final_state_file = open('Levels/Final States/' + level_name, 'r')
+        final_map = final_state_file.read().split(sep='\n')
+        final_state_file.close()
+        final_player = None
+        final_boxes = list()
+        for y in range(self.map_rows):
+            for x in range(self.map_cols):
+                if final_map[y][x] == '@':
+                    final_player = (x, y)
+                elif final_map[y][x] == '+':
+                    final_boxes.append((x, y))
+        final_state = State(final_player, final_boxes)
+
+        O_final = Queue()
+        O_final.put(final_state)
+        C_final = {final_state}
+
+        directions = ['up', 'down', 'right', 'left']
+
+        while not O_start.empty() or not O_final.empty():
+            self.iteration_count += 1
+            if not O_start.empty():
+                state = O_start.get()
+
+                if state in C_final:
+                    self.O_end_node_count = O_start.qsize() + O_final.qsize()
+                    C_final_list = list(C_final)
+                    second_state = C_final_list.pop(C_final_list.index(state))
+                    return self.Connect_Ways(state, second_state)
+
+                for direction in directions:
+                    new_state = self.Check_Direction(direction, state)
+                    if new_state != None:
+                        if not new_state in C_start:
+                            O_start.put(new_state)
+                            C_start.add(new_state)
+                            O_size = O_start.qsize() + O_final.qsize()
+                            self.max_node_count = max(self.max_node_count, len(C_start) + len(C_final) + O_size)
+                            self.O_max_node_count = max(self.O_max_node_count, O_size)
+            
+            if not O_final.empty():
+                state = O_final.get()
+
+                if state in C_start:
+                    self.O_end_node_count = O_start.qsize() + O_final.qsize()
+                    C_start_list = list(C_start)
+                    second_state = C_start_list.pop(C_start_list.index(state))
+                    return self.Connect_Ways(second_state, state)
+
+                for direction in directions:
+                    new_state = self.Check_Direction_Backwards(direction, state)
+                    if new_state != None:
+                        if not new_state in C_final:
+                            O_final.put(new_state)
+                            C_final.add(new_state)
+                            O_size = O_start.qsize() + O_final.qsize()
+                            self.max_node_count = max(self.max_node_count, len(C_start) + len(C_final) + O_size)
+                            self.O_max_node_count = max(self.O_max_node_count, O_size)
+        
+        return None
+
+    def Connect_Ways(self, state_start_end, state_final_end):
+        flipper = LifoQueue()
+        while state_final_end.prev_state != None:
+            flipper.put(state_final_end)
+            state_final_end = state_final_end.prev_state
+        flipper.put(state_final_end)
+         
+        maps = list()
+        while not flipper.empty():
+            self.steps_counter += 1
+            state = flipper.get()
+            map = self.Create_Map(state)
+            maps.append(map)
+
+        state = state_start_end.prev_state 
+        while state.prev_state != None:
+            self.steps_counter += 1
+            map = self.Create_Map(state)
+            maps.append(map)
+            state = state.prev_state
+        map = self.Create_Map(state)
+        maps.append(map)
+        
+        return maps
+
     def Check_Direction(self, direction, state):
         delta = {
             'up':    (0, -1),
@@ -217,6 +438,34 @@ class Game:
             boxes.add((nnx, nny))
 
         return State((nx, ny), boxes, state)
+
+    def Check_Direction_Backwards(self, direction, state):
+        delta = {
+            'up':    (0, -1),
+            'down':  (0,  1),
+            'left':  (-1, 0),
+            'right': (1,  0)
+        }
+        dx, dy = delta[direction]
+        x, y = state.player
+        px, py = x - dx, y - dy
+        bx, by = x + dx, y + dy
+
+        if not (0 <= px < self.map_cols and 0 <= py < self.map_rows):
+            return None
+
+        if self.map[py][px] == '#':
+            return None
+
+        boxes = set(state.boxes)
+        if (px, py) in boxes:
+            return None
+
+        if (bx, by) in boxes:
+            boxes.remove((bx, by))
+            boxes.add((x, y))
+        
+        return State((px, py), boxes, state)
 
     def Create_Map(self, state):
         map = [list(row) for row in self.map]
@@ -279,17 +528,16 @@ class Game:
         level_rect = level.get_rect(center=(center_x, self.height // 3))
         self.surface.blit(level, level_rect)
 
-        algo_text = self.font.render(f"Алгоритм: {self.structure_type}", True, (0, 0, 0))
+        algo_text = self.font.render(f"{self.algorithms[self.algorithm_index]}", True, (0, 0, 0))
         algo_rect = algo_text.get_rect(center=(center_x, self.height // 3 * 2))
         self.surface.blit(algo_text, algo_rect)
 
     def Draw_Stats(self):
         self.surface.fill((62, 180, 137))
         center_x = self.width // 2
-        algo_type = 'Поиск в ширину' if self.structure_type == 'queue' else 'Поиск в глубину'
 
         lines = [
-            f"Алгоритм: {algo_type}",
+            f"Алгоритм: {self.algorithms[self.algorithm_index]}",
             f"Количество итераций: {self.iteration_count}",
             f"Максимальное количество узлов в O: {self.O_max_node_count}",
             f"Конечное количество узлов в O: {self.O_end_node_count}",
